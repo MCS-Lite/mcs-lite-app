456acaf97f744af4ad08032ecc72dc7ce3bcb63f
po-ju.chen <tonyone0902@gmail.com>
2017-02-15 21:09:26 +0800

    add breadcrumbs into dashboard layout

--- client/app/components/dashboard/index.js
+++ client/app/components/dashboard/index.js
@@ -4,13 +4,18 @@ import Footer from '../footer';
 import Header from '../header';
 import c from 'classnames';
 
+import dashboardStyles from './dashboard.css';
+import Breadcrumb from './breadcrumb';
+
 const Dashboard = ({ ...props }) => {
   return (
     <div>
       <Header
         logoutFn={()=>{}}
         imageUrl='http://img.mediatek.com/150/mtk.linkit/profile/3492e14e-f0fb-4718-a9a7-a49e95d8cb30.jpeg'/>
-      123123
+      <div className={dashboardStyles.base}>
+        <Breadcrumb />
+      </div>
       <Footer />
     </div>
   );
---
+++ client/app/components/dashboard/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  'Dashboard.dashboard': {
+    id: 'Dashboard.dashboard',
+    description: '',
+    defaultMessage: '儀表板',
+  },
+});
+
+export default messages;
