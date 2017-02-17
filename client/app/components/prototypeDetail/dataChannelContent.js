3680e9e680cb26bd4dc1934e279ae5042590cedb
po-ju.chen <tonyone0902@gmail.com>
2017-02-17 11:00:04 +0800

    no message

--- client/app/components/prototypeDetail/dataChannelContent.js
+++ client/app/components/prototypeDetail/dataChannelContent.js
@@ -3,12 +3,32 @@ import React, { Component } from 'react';
 import { default as compose } from 'recompose/compose';
 import { default as pure } from 'recompose/pure';
 import NewDisplayCard from './newDisplayCard';
+import { DataChannelCard, DataChannelAdapter } from 'mcs-lite-ui';
 
 
 const DataChannelContentLayout = ({ prototypeId }) => {
   return (
     <div>
       <NewDisplayCard prototypeId={prototypeId} />
+      <DataChannelCard
+        data-width="half"
+        title="Title"
+        subtitle="Last data point time : 2015-06-12 12:00"
+        description="You can input description of controller here. You can input description of You can input description of controller here. You can input description of"
+        header={<a href="">Link</a>}
+      >
+        <DataChannelAdapter
+          dataChannelProps={{
+            id: 'Integer Control id',
+            type: 'Integer_Control',
+            values: { value: 50 },
+            format: {
+              unit: 'ampere',
+            },
+          }}
+          eventHandler={console.log} // eslint-disable-line
+        />
+      </DataChannelCard>
     </div>
   );
 }
