b3c38e5b3898e03bedbb5e70e836c5a57955dc59
Hau Chen <cettoana@gmail.com>
2017-02-21 17:52:01 +0800

    i18n(DeviceList): set defaultMessage to traditional chinese

---
+++ client/app/components/common/dialogs/messages.js
@@ -0,0 +1,22 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  'Dialogs.deviceNameLabel': {
+    id: 'Dialogs.deviceNameLabel',
+    defaultMessage: '測試裝置名稱',
+  },
+  'Dialogs.deviceNamePlaceholder': {
+    id: 'Dialogs.deviceNamePlaceholder',
+    defaultMessage: '輸入裝置名稱',
+  },
+  'Dialogs.descriptionLabel': {
+    id: 'descriptionLabel',
+    defaultMessage: '描述',
+  },
+  'Dialogs.descriptionPlaceholder': {
+    id: 'Dialogs.descriptionPlaceholder',
+    defaultMessage: '輸入裝置描述',
+  },
+});
+
+export default messages;
