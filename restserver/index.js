61ecd347986a4ac935c0678921b50b0aabbc9c14
blue chen <tonyone0902@gmail.com>
2017-03-09 09:55:40 +0800

    add deviceKey

--- restserver/index.js
+++ restserver/index.js
@@ -33,7 +33,7 @@ app.set('views', path.resolve(__dirname, '../client'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, deviceKey");
   next();
 });
 
