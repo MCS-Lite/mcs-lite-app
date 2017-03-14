222431d3cbfb4b0c721bf9373d237973376d1b6a
po-ju.chen <tonyone0902@gmail.com>
2017-03-14 10:06:13 +0800

    adjust UI and format content

--- client/app/components/dataChannelCards/mcsDisplayCardInteger/format.js
+++ client/app/components/dataChannelCards/mcsDisplayCardInteger/format.js
@@ -3,14 +3,4 @@ module.exports = {
     displayType: 'unitType',
     required: true,
   },
-  lowerbound: {
-    displayType: 'text',
-    required: true,
-    type: 'number',
-  },
-  upperbound: {
-    displayType: 'text',
-    required: true,
-    type: 'number',
-  },
 };
