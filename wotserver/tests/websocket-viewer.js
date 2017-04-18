1a7a6349aa75ed30799656a3ea08caffa54ad91a
po-ju.chen <tonyone0902@gmail.com>
2017-04-18 11:23:14 +0800

    add test for wotserver

--- wotserver/tests/websocket-viewer.js
+++ wotserver/tests/websocket-viewer.js
@@ -23,27 +23,6 @@ client.on('connect', function(connection) {
             console.log("Received: '" + message.utf8Data + "'");
         }
     });
-
-    function sendNumber() {
-        if (connection.connected) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            // 1 to 10
-            // var lucky = Math.round(Math.random() * 40 + 1);
-            //var obj = {ax: number.toString(), ay: 0, az: 0};
-            var obj = {
-                datachannelId: datachannelId,
-                values: {
-                    value: number,
-                }
-            };
-
-            console.log('Pushing: ' + JSON.stringify(obj));
-
-            connection.sendUTF(JSON.stringify(obj));
-            setTimeout(sendNumber, 500);
-        }
-    }
-    sendNumber();
 });
 
-client.connect('ws://localhost:8000/deviceId/' + deviceId + '/deviceKey/' + deviceKey, '');
+client.connect('ws://localhost:8000/deviceId/' + deviceId + '/deviceKey/' + deviceKey + '/viewer', '');
