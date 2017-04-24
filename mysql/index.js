5ea3370a784d7c238d7872d04399964171a2321d
po-ju.chen <tonyone0902@gmail.com>
2017-04-24 17:00:03 +0800

    add mysql db wrapper

---
+++ mysql/index.js
@@ -0,0 +1,32 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('mcs-lite', 'username', 'password');
+
+var $users = sequelize.define('users', require('../users/schema')(Sequelize));
+var $devices = sequelize.define('devices', require('../devices/schema')(Sequelize));
+var $datapoints = sequelize.define('datapoints', require('../datapoints/schema')(Sequelize));
+var $prototypes = sequelize.define('prototypes', require('../prototypes/schema')(Sequelize));
+var $datachannels = sequelize.define('datachannels', require('../datachannels/schema')(Sequelize));
+var $unittypes = sequelize.define('unittypes', require('../unittypes/schema')(Sequelize));
+
+
+var users = require('./users/index')($users);
+var devices = require('./devices/index')($devices);
+var datapoints = require('./datapoints/index')($datapoints);
+var prototypes = require('./prototypes/index')($prototypes);
+var datachannels = require('./datachannels/index')($datachannels);
+var unittypes = require('./unittypes/index')($unittypes);
+
+function init(host, port, config) {
+  return {
+    prototypes: prototypes,
+    devices: devices,
+    datapoints: datapoints,
+    users: users,
+    datachannels: datachannels,
+    unittypes: unittypes,
+  };
+};
+
+module.exports = {
+  init: init,
+};
