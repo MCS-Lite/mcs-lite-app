5cdd5ac59c63183f8319add0103c8c3ea759ae89
iamblue <tonyone0902@gmail.com>
2017-05-09 17:35:20 +0800

    add migration.js setting

--- mysql/datapoints/schema.js
+++ mysql/datapoints/schema.js
@@ -1,6 +1,39 @@
 module.exports = function(Sequelize) {
   return {
-    username: Sequelize.STRING,
-    birthday: Sequelize.DATE
+    id: {
+      type: Sequelize.BIGINT,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    deviceId: { 
+      type: Sequelize.STRING, 
+      allowNull: false,
+    },
+    deviceKey: { 
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    datachannelId: { 
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    values: { 
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    updatedAt: { 
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false,
+    },
+    isActive: { 
+      type: Sequelize.BOOLEAN, 
+      allowNull: false,
+    },
   };
 };
---
+++ mysql/migrations/20170509112716-devices.js
@@ -0,0 +1,71 @@
+module.exports = {
+  up: function(migration, DataTypes, done){
+    migration.createTable('devices', {
+      id: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      createUserId: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deviceId: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+      },
+      deviceKey: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deviceName: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deviceDescription: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      deviceImageURL: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      prototypeId: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      isHeartbeating: { 
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      isPublic: { 
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      fwId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      isActive: { 
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      }
+    }, {
+      charset: 'utf8'
+    });
+    done();
+  },
+  down: function(migration, DataTypes, done){
+    done();
+  }
+};
