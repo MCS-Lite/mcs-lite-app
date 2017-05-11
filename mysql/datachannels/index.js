6a307b180efbd37ace160419a2e0bba038ed9600
iamblue <tonyone0902@gmail.com>
2017-05-11 16:14:04 +0800

    mysql setting

--- mysql/datachannels/index.js
+++ mysql/datachannels/index.js
@@ -4,109 +4,114 @@ var schema = require('./schema');
 
 module.exports = function(datachannels) {
 
-  return {
-    validateSchema: function(object) {
-      return v.validate(object, schema);
-    },
+  // return {
+  //   validateSchema: function(object) {
+  //     return v.validate(object, schema);
+  //   },
 
     retrievDatachannel: function(field) {
       field.isActive = true;
       return new Promise(function(resolve, reject) {
-        return datachannels.find(field, function(err, data) {
-          if (err) return reject();
-          return resolve(data);
-        });
-      });
-    },
-
-    addNewDatachannel: function(field) {
-      field.updatedAt = new Date().getTime();
-      field.createdAt = new Date().getTime();
-      field.isActive = true;
-
-      return new Promise(function(resolve, reject) {
-        return prototypes.find({
-          prototypeId: field.prototypeId,
-        }, function(err, data) {
-          if (err) return reject();
-          if (data.length === 1) {
-            return resolve(data[0]);
-          } else {
-            return reject({ error: 'Can not find this prototypeId.' });
-          }
+        return datachannels
+        .find(field)
+        .success(function(data) {
+          if (!data) data = [];
+          return resolve(data);            
         })
-      })
-      .then(function() {
-        return new Promise(function(resolve, reject) {
-          var validataSchema = v.validate(field, schema);
-
-          if (validataSchema.errors.length === 0) {
-            return resolve();
-          } else {
-            return reject({ schema: validataSchema.errors })
-          }
-        });
-      })
-      .then(function() {
-        return new Promise(function(resolve, reject) {
-          return datachannels.insert(field, function(err, data) {
-            if (err) return reject();
-            return resolve(data);
-          });
+        .error(function(err) {
+          return reject(err);
         });
       });
     },
 
-    editDatachannel: function(query, update) {
-      return new Promise(function(resolve, reject) {
-        return datachannels.update(query, { $set: update }, {}, function(err, num) {
-          if (err) return reject();
-          resolve({ message: 'success.' });
-        });
-      });
-    },
+  //   addNewDatachannel: function(field) {
+  //     field.updatedAt = new Date().getTime();
+  //     field.createdAt = new Date().getTime();
+  //     field.isActive = true;
 
-    deleteDatachannel: function(query) {
-      return new Promise(function(resolve, reject) {
-        return datachannels.update(query, { $set: { updatedAt: new Date().getTime(), isActive: false } }, {}, function(err, num) {
-          if (err) return reject();
-          resolve({ message: 'success.' });
-        });
-      });
-    },
+  //     return new Promise(function(resolve, reject) {
+  //       return prototypes.find({
+  //         prototypeId: field.prototypeId,
+  //       }, function(err, data) {
+  //         if (err) return reject();
+  //         if (data.length === 1) {
+  //           return resolve(data[0]);
+  //         } else {
+  //           return reject({ error: 'Can not find this prototypeId.' });
+  //         }
+  //       })
+  //     })
+  //     .then(function() {
+  //       return new Promise(function(resolve, reject) {
+  //         var validataSchema = v.validate(field, schema);
 
-    cloneDatachannel: function(fromPrototypeId, toPrototypeId, userId) {
-      var _this = this;
-      return new Promise(function(resolve, reject) {
-        return datachannels.find({
-            prototypeId: fromPrototypeId,
-            isActive: true,
-          }, function(err, data) {
-          if (err) return reject();
-          resolve(data);
-        });
-      })
-      .then(function(data) {
-        var datachannelPromise = []
-        data.forEach(function(k, v) {
-          k.prototypeId = toPrototypeId;
-          k.createUserId = userId;
-          delete k._id;
-          datachannelPromise.push(_this.addNewDatachannel(k));
-        });
-        return Promise.all(datachannelPromise)
-      });
-    },
+  //         if (validataSchema.errors.length === 0) {
+  //           return resolve();
+  //         } else {
+  //           return reject({ schema: validataSchema.errors })
+  //         }
+  //       });
+  //     })
+  //     .then(function() {
+  //       return new Promise(function(resolve, reject) {
+  //         return datachannels.insert(field, function(err, data) {
+  //           if (err) return reject();
+  //           return resolve(data);
+  //         });
+  //       });
+  //     });
+  //   },
 
-    importDataChannel: function(datachannel, userId, prototypeId, isMCSLite) {
-      var _this = this;
-      var channelPool = [];
-      datachannel.forEach(function(key, index) {
-        key.prototypeId = prototypeId;
-        key.createUserId = userId;
-        channelPool.push(_this.addNewDatachannel(key));
-      });
-      return Promise.all(channelPool)
-    },
-  };
+  //   editDatachannel: function(query, update) {
+  //     return new Promise(function(resolve, reject) {
+  //       return datachannels.update(query, { $set: update }, {}, function(err, num) {
+  //         if (err) return reject();
+  //         resolve({ message: 'success.' });
+  //       });
+  //     });
+  //   },
+
+  //   deleteDatachannel: function(query) {
+  //     return new Promise(function(resolve, reject) {
+  //       return datachannels.update(query, { $set: { updatedAt: new Date().getTime(), isActive: false } }, {}, function(err, num) {
+  //         if (err) return reject();
+  //         resolve({ message: 'success.' });
+  //       });
+  //     });
+  //   },
+
+  //   cloneDatachannel: function(fromPrototypeId, toPrototypeId, userId) {
+  //     var _this = this;
+  //     return new Promise(function(resolve, reject) {
+  //       return datachannels.find({
+  //           prototypeId: fromPrototypeId,
+  //           isActive: true,
+  //         }, function(err, data) {
+  //         if (err) return reject();
+  //         resolve(data);
+  //       });
+  //     })
+  //     .then(function(data) {
+  //       var datachannelPromise = []
+  //       data.forEach(function(k, v) {
+  //         k.prototypeId = toPrototypeId;
+  //         k.createUserId = userId;
+  //         delete k._id;
+  //         datachannelPromise.push(_this.addNewDatachannel(k));
+  //       });
+  //       return Promise.all(datachannelPromise)
+  //     });
+  //   },
+
+  //   importDataChannel: function(datachannel, userId, prototypeId, isMCSLite) {
+  //     var _this = this;
+  //     var channelPool = [];
+  //     datachannel.forEach(function(key, index) {
+  //       key.prototypeId = prototypeId;
+  //       key.createUserId = userId;
+  //       channelPool.push(_this.addNewDatachannel(key));
+  //     });
+  //     return Promise.all(channelPool)
+  //   },
+  // };
 };
