7e96100b9932101bdcd9c324623aeb0ee2c3dba3
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-05-24 13:01:58 +0800

    feat(packages): upgrade mcs-lite-* packages

--- client/app/components/deviceDetail/apiHint/fetch.js
+++ client/app/components/deviceDetail/apiHint/fetch.js
@@ -1,4 +1,11 @@
-import { createFetch, base, parse, method as hcMethod, params } from 'http-client';
+/* global window */
+import {
+  createFetch,
+  base,
+  parse,
+  method as hcMethod,
+  params,
+} from 'http-client';
 
 /**
  * Handle response - throw or just return
@@ -13,9 +20,7 @@ function handleResponse(response) {
   return data;
 }
 
-const fetchAPIHint = (
-  deviceId, datachannel, language, deviceKey, method,
-) => {
+const fetchAPIHint = (deviceId, datachannel, language, deviceKey, method) => {
   const fetch = createFetch(
     base(window.apiUrl),
     hcMethod('GET'),
@@ -23,8 +28,9 @@ const fetchAPIHint = (
     parse('json', 'jsonData'),
   );
 
-  return fetch(`/devices/${deviceId}/datachannels/${datachannel.datachannelId}/datachanneltypes/${datachannel.channelType.id}/methods/${method}/types/${datachannel.type}/contents/${language}`)
-    .then(handleResponse);
+  return fetch(
+    `/devices/${deviceId}/datachannels/${datachannel.datachannelId}/datachanneltypes/${datachannel.channelType.id}/methods/${method}/types/${datachannel.type}/contents/${language}`,
+  ).then(handleResponse);
 };
 
 export default fetchAPIHint;
