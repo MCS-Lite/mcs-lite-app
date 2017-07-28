a832d797d391292701c1cda902b8a91fbadd72e2
iamblue <tonyone0902@gmail.com>
2017-07-28 12:17:44 +0800

    fixed first login cannot fetch locale problem

--- client/app/containers/app.js
+++ client/app/containers/app.js
@@ -9,30 +9,30 @@ const qs = query(window.location.search.substr(1).split('&'));
 class App extends Component {
   componentWillMount() {
     const { checkToken: doCheckToken, location } = this.props;
+    if (!/locale\=/.test(window.location.search)) {
+      let language;
+
+      if (localStorage.getItem('locale')) {
+        language = localStorage.getItem('locale');
+      } else {
+        language = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage);
+        localStorage.setItem('locale', language);
+        if (/^en/.test(language)) language = 'en';
+      }
+
+      if (!/^(en|zh-tw|zh-cn)$/.test(language.toLowerCase())) language = 'en';
+
+      if (!/\?/.test(window.location.href)) {
+        window.location.href += '?locale=' + language;
+      } else {
+        window.location.href += '&locale=' + language;
+      }
+    } else {
+      localStorage.setItem('locale', qs['locale']);
+    }
+    
     if (!/(login)|(signup)/.test(location.pathname)) {
       doCheckToken();
-    } else {
-      if (!/locale\=/.test(window.location.search)) {
-        let language;
-
-        if (localStorage.getItem('locale')) {
-          language = localStorage.getItem('locale');
-        } else {
-          language = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage);
-          localStorage.setItem('locale', language);
-          if (/^en/.test(language)) language = 'en';
-        }
-
-        if (!/^(en|zh-tw|zh-cn)$/.test(language.toLowerCase())) language = 'en';
-
-        if (!/\?/.test(window.location.href)) {
-          window.location.href += '?locale=' + language;
-        } else {
-          window.location.href += '&locale=' + language;
-        }
-      } else {
-        localStorage.setItem('locale', qs['locale']);
-      }
     }
   }
 
