c4bd95993cd6fd1896c19e9d83aa3f241a7592d3
Daria Chen <smartexist@gmail.com>
2017-08-01 18:58:43 +0800

    feat(travis): trigger downstream job.

---
+++ trigger-dependent-build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# pass the commit ID to downstream job
+curl -X PATCH \
+  -H "Content-Type: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${GIT_TOKEN}" \
+  -d '{ "env_var.value": "${TRAVIS_COMMIT}", "env_var.public": true }' \
+  https://api.travis-ci.org/repo/MCS-Lite%2Fmcs-lite-jmeter-test/env_var/efce64d3-827b-4631-a3c4-f931815dcc32
+
+# pass the build number to downstream job
+curl -X PATCH \
+  -H "Content-Type: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${GIT_TOKEN}" \
+  -d '{ "env_var.value": "${TRAVIS_BUILD_NUMBER}", "env_var.public": true }' \
+  https://api.travis-ci.org/repo/MCS-Lite%2Fmcs-lite-jmeter-test/env_var/3f5d2fc1-dcbf-4fd6-9c9a-6078db906739
+
+# trigger downstream test job
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token ${GIT_TOKEN}" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/MCS-Lite%2Fmcs-lite-jmeter-test/requests
+
