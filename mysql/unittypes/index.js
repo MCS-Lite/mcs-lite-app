4138142e00fffdb397cfa99de31a48091f636c16
iamblue <tonyone0902@gmail.com>
2017-09-04 14:30:44 +0800

    add mysql connect

--- mysql/unittypes/index.js
+++ mysql/unittypes/index.js
@@ -1,2 +1,49 @@
+// var Validator = require('jsonschema').Validator;
+// var v = new Validator();
+
+// var schema = require('./schema');
+
 module.exports = function(unittypes) {
-};
+  return {
+    retrieveUnitTypes: function(query) {
+      query.isActive = true;
+
+      return new Promise(function(resolve, reject) {
+        return unittypes.find({ where: query })
+        .success(function(data) {
+          return resolve(data);
+        })
+        .error(function(data) {
+          if (err) return reject();
+        });
+      });
+    },
+    addNewUnitTypes: function(field) {
+      field.createdAt = new Date().getTime();
+      field.updatedAt = new Date().getTime();
+
+      // return new Promise( function(resolve, reject) {
+      //   /* validate schema */
+      //   var validataSchema = v.validate(field, schema);
+
+      //   if (validataSchema.errors.length === 0) {
+      //     return resolve();
+      //   } else {
+      //     return reject({ schema: validataSchema.errors })
+      //   }
+      // })
+      // .then(function() {
+        return new Promise(function(resolve, reject) {
+          return unittypes
+          .create(field)
+          .sucess(function(data) {
+            return resolve(data);
+          })
+          .error(function(err) {
+            if (err) return reject();            
+          });
+        });
+      // });
+    },
+  };
+}
