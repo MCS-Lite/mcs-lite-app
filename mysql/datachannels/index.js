21824564020a77ba4fc232251281765d49dc8e77
iamblue <tonyone0902@gmail.com>
2017-09-04 14:30:44 +0800

    add mysql connect

--- mysql/datachannels/index.js
+++ mysql/datachannels/index.js
@@ -1,22 +1,20 @@
-var Validator = require('jsonschema').Validator;
-var v = new Validator();
-var schema = require('./schema');
+// var Validator = require('jsonschema').Validator;
+// var v = new Validator();
+// var schema = require('./schema');
 
-module.exports = function(datachannels) {
+module.exports = function(datachannels, prototypes) {
 
   return {
-  //   validateSchema: function(object) {
-  //     return v.validate(object, schema);
-  //   },
+    // validateSchema: function(object) {
+    //   return v.validate(object, schema);
+    // },
 
     retrievDatachannel: function(field) {
       field.isActive = true;
       return new Promise(function(resolve, reject) {
-        return datachannels
-        .find(field)
+        return datachannels.find({ where: field })
         .success(function(data) {
-          if (!data) data = [];
-          return resolve(data);            
+          return resolve(data);
         })
         .error(function(err) {
           return reject(err);
@@ -24,94 +22,121 @@ module.exports = function(datachannels) {
       });
     },
 
-  //   addNewDatachannel: function(field) {
-  //     field.updatedAt = new Date().getTime();
-  //     field.createdAt = new Date().getTime();
-  //     field.isActive = true;
+    addNewDatachannel: function(field) {
+      field.updatedAt = new Date().getTime();
+      field.createdAt = new Date().getTime();
+      field.isActive = true;
 
-  //     return new Promise(function(resolve, reject) {
-  //       return prototypes.find({
-  //         prototypeId: field.prototypeId,
-  //       }, function(err, data) {
-  //         if (err) return reject();
-  //         if (data.length === 1) {
-  //           return resolve(data[0]);
-  //         } else {
-  //           return reject({ error: 'Can not find this prototypeId.' });
-  //         }
-  //       })
-  //     })
-  //     .then(function() {
-  //       return new Promise(function(resolve, reject) {
-  //         var validataSchema = v.validate(field, schema);
+      return new Promise(function(resolve, reject) {
+        return prototypes.find({
+          where: {
+            prototypeId: field.prototypeId,
+          },
+        })
+        .success(function(data) {
+          if (data.length === 1) {
+            return resolve(data[0]);
+          } else {
+            return reject({ error: 'Can not find this prototypeId.' });
+          }
+        })
+        .error(function(err) {
+          return reject(err);
+        });
+      })
+      // .then(function() {
+      //   return new Promise(function(resolve, reject) {
+      //     var validataSchema = v.validate(field, schema);
 
-  //         if (validataSchema.errors.length === 0) {
-  //           return resolve();
-  //         } else {
-  //           return reject({ schema: validataSchema.errors })
-  //         }
-  //       });
-  //     })
-  //     .then(function() {
-  //       return new Promise(function(resolve, reject) {
-  //         return datachannels.insert(field, function(err, data) {
-  //           if (err) return reject();
-  //           return resolve(data);
-  //         });
-  //       });
-  //     });
-  //   },
+      //     if (validataSchema.errors.length === 0) {
+      //       return resolve();
+      //     } else {
+      //       return reject({ schema: validataSchema.errors })
+      //     }
+      //   });
+      // })
+      .then(function() {
+        return new Promise(function(resolve, reject) {
+          return datachannels
+          .create(field)
+          .success(function(data) {
+            return resolve(data);
+          })
+          .error(function(err) {
+            return reject(err);
+          });
+        });
+      });
+    },
 
-  //   editDatachannel: function(query, update) {
-  //     return new Promise(function(resolve, reject) {
-  //       return datachannels.update(query, { $set: update }, {}, function(err, num) {
-  //         if (err) return reject();
-  //         resolve({ message: 'success.' });
-  //       });
-  //     });
-  //   },
+    editDatachannel: function(query, update) {
+      return new Promise(function(resolve, reject) {
+        return datachannels
+        .update(update, { where: query })
+        .success(function(num) {
+          return resolve({ message: 'success.' });
+        })
+        .error(function(err) {
+          return reject();
+        });
+      });
+    },
 
-  //   deleteDatachannel: function(query) {
-  //     return new Promise(function(resolve, reject) {
-  //       return datachannels.update(query, { $set: { updatedAt: new Date().getTime(), isActive: false } }, {}, function(err, num) {
-  //         if (err) return reject();
-  //         resolve({ message: 'success.' });
-  //       });
-  //     });
-  //   },
+    deleteDatachannel: function(query) {
+      return new Promise(function(resolve, reject) {
+        return datachannels
+        .update({ 
+          updatedAt: new Date().getTime(), 
+          isActive: false,
+        }, { 
+          where: query 
+        })
+        .success(function() {
+          return resolve({ message: 'success.' });          
+        })
+        .error(function(err) {
+          return reject();
+        });
+      });
+    },
 
-  //   cloneDatachannel: function(fromPrototypeId, toPrototypeId, userId) {
-  //     var _this = this;
-  //     return new Promise(function(resolve, reject) {
-  //       return datachannels.find({
-  //           prototypeId: fromPrototypeId,
-  //           isActive: true,
-  //         }, function(err, data) {
-  //         if (err) return reject();
-  //         resolve(data);
-  //       });
-  //     })
-  //     .then(function(data) {
-  //       var datachannelPromise = []
-  //       data.forEach(function(k, v) {
-  //         k.prototypeId = toPrototypeId;
-  //         k.createUserId = userId;
-  //         delete k._id;
-  //         datachannelPromise.push(_this.addNewDatachannel(k));
-  //       });
-  //       return Promise.all(datachannelPromise)
-  //     });
-  //   },
+    cloneDatachannel: function(fromPrototypeId, toPrototypeId, userId) {
+      var _this = this;
+      return new Promise(function(resolve, reject) {
+        return datachannels.find({
+          where: {
+            prototypeId: fromPrototypeId,
+            isActive: true,
+          }
+        })
+        .success(function(data) {
+          return resolve(data);
+        })
+        .error(function(err) {
+          return reject(err);
+        });
+      })
+      .then(function(data) {
+        var datachannelPromise = []
+        data.forEach(function(k, v) {
+          k.prototypeId = toPrototypeId;
+          k.createUserId = userId;
+          delete k._id;
+          datachannelPromise.push(_this.addNewDatachannel(k));
+        });
+        return Promise.all(datachannelPromise)
+      });
+    },
 
-  //   importDataChannel: function(datachannel, userId, prototypeId, isMCSLite) {
-  //     var _this = this;
-  //     var channelPool = [];
-  //     datachannel.forEach(function(key, index) {
-  //       key.prototypeId = prototypeId;
-  //       key.createUserId = userId;
-  //       channelPool.push(_this.addNewDatachannel(key));
-  //     });
-  //     return Promise.all(channelPool)
-  //   },
+    importDataChannel: function(datachannel, userId, prototypeId, isMCSLite) {
+      var _this = this;
+      var channelPool = [];
+      datachannel.forEach(function(key, index) {
+        key.prototypeId = prototypeId;
+        key.createUserId = userId;
+        channelPool.push(_this.addNewDatachannel(key));
+      });
+      return Promise.all(channelPool)
+    },
   };
-};
+}
--- mysql/users/index.js
+++ mysql/users/index.js
@@ -1,2 +1,229 @@
+// var Validator = require('jsonschema').Validator;
+// var v = new Validator();
+// var schema = require('./schema');
+var shortid = require('shortid');
+var crypto = require('crypto');
+// var secretKey = require('../../configs/rest').secretKey;
+
 module.exports = function(users) {
-};
+  return {
+    // validateSchema: function(object) {
+    //   return v.validate(object, schema);
+    // },
+    changePassword: function(userId, password) {
+      password = crypto
+        .createHmac('sha256', secretKey)
+        .update(password)
+        .digest('hex');
+
+      return new Promise(function(resolve, reject) {
+        return users
+        .update({ 
+          password: password 
+        }, {
+          where: {
+            userId: userId,
+            isActive: true,
+          },
+        })
+        .success(function(num) {
+          return resolve({ message: 'success' });
+        })
+        .error(function() {
+          if (err) return reject();
+        });
+      });
+    },
+
+    signInUser: function(email, password, admin) {
+      // admin is for admin console login 
+      password = crypto
+        .createHmac('sha256', secretKey)
+        .update(password)
+        .digest('hex');
+
+      var query = {
+        email: email,
+        password: password,
+        isActive: true,
+      };
+
+      if (admin) {
+        query.isAdmin = true;
+      }
+
+      return new Promise( function(resolve, reject) {
+        return users
+        .find({ where: query })
+        .success(function(err, data) {
+          if (data.length === 1) {
+            return resolve(data[0]);
+          } else {
+            return reject({ error: 'email / password is incorrect.' });
+          }
+        })
+        .error(function(err) {
+          if (err) {
+            return reject(err);
+          }
+        })
+      });
+    },
+
+    addNewUser: function(field) {
+      field.userId = shortid.generate();
+      field.createdAt = new Date().getTime();
+      field.updatedAt = new Date().getTime();
+      field.isActive = true;
+      field.userImage = '';
+      field.password = crypto
+        .createHmac('sha256', secretKey)
+        .update(field.password)
+        .digest('hex');
+
+      // var validataSchema = v.validate(field, schema);
+
+      // return new Promise( function(resolve, reject) {
+      //   /* validate schema */
+      //   var validataSchema = v.validate(field, schema);
+
+      //   if (validataSchema.errors.length === 0) {
+      //     return resolve();
+      //   } else {
+      //     return reject({ schema: validataSchema.errors })
+      //   }
+      // })
+      // .then(function() {
+        /* find same email or not */
+        return new Promise( function(resolve, reject) {
+          return users.find({
+            where: { 
+              email: field.email,
+            },
+          }, function(err, data) {
+            if (err) {
+              return reject(err);
+            }
+            if (data.length === 0) {
+              return resolve();
+            } else {
+              return reject({ error: 'This email was registed!' });
+            }
+          });
+        // });
+      })
+      .then(function() {
+        /* inser into database */
+        return new Promise(function(resolve, reject) {
+          return users
+          .create(field)
+          .success(function(data) {
+            return resolve(data);
+          })
+          .error(function(err) {
+            if (err) return reject();
+          });
+        });
+      });
+    },
+
+    retrieveUserList: function() {
+      return new Promise(function(resolve, reject) {
+        return users.findAll({})
+        .success(function(data) {
+          return resolve(data);
+        })
+        .error(function(err) {
+          if (err) return reject();          
+        })
+      });
+    },
+
+    checkIsAdmin: function(userId, isMiddleware) {
+      return new Promise(function(resolve, reject) {
+        return users.find({ 
+          where: {
+            userId: userId, 
+            isAdmin: true, 
+            isActive: true 
+          }
+        })
+        .success(function(data) {
+          if (isMiddleware) {
+            return resolve(data);
+          }
+
+          if (data.length != 1) {
+            return reject({ error: 'This user is not admin!' });
+          } else {
+            return resolve(data);
+          }
+        })
+        .error(function(err) {
+          if (err) return reject();          
+        })
+      });
+    },
+
+    checkDefaultUserCount: function() {
+      return new Promise(function(resolve, reject) {
+        return users.find({ 
+          where: {
+            isActive: true,          
+          },
+        })
+        .success(function(data) {
+          if (data.length !== 0) {
+            return resolve(false);  
+          }
+          return resolve(true);  
+        })
+        .error(function(err) {
+          if (err) return reject();          
+        });
+      });
+    },
+
+    changeUserToAdmin: function(email, isAdmin) {
+      return new Promise(function(resolve, reject) {
+        return users.update({ isAdmin: isAdmin }, {
+          where: {
+            email: email,
+            isActive: true,  
+          }
+        })
+        .success(function(num) {
+          return resolve({ message: 'success' });
+        })
+        .error(function(err) {
+          if (err) return reject();          
+        });
+      });
+    },
+
+    retrieveOneUser: function(query) {
+      return new Promise(function(resolve, reject) {
+        return users
+        .find(query)
+        .success(function(data) {
+          return resolve(data);
+        })
+        .error(function(err) {
+          if (err) return reject();
+        });
+      });
+    },
+
+    editUser: function(query, update) {
+      return new Promise(function(resolve, reject) {
+        return users.update(update, { where: query })
+        .success(function(num) {
+          return resolve({ message: 'success' });
+        })
+        .error(function(err) {
+          if (err) return reject();          
+        });
+      });
+    },
+  };
+}
