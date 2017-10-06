180b037d16badc5d0c72b92b9fef182bfcce5969
Hau Chen <cettoana@gmail.com>
2017-10-06 16:14:34 +0800

    chore(babel): introduce babel for server side code

--- adminServer/controllers/service.js
+++ adminServer/controllers/service.js
@@ -1,131 +1,143 @@
-var os = require('os');
-var fs = require('fs');
-var path = require('path');
-var exec = require('child_process').exec;
-var kill = require('cross-port-killer').kill;
-var $rest = require('../../configs/rest');
-var $wot = require('../../configs/wot');
-var $stream = require('../../configs/stream');
-var $admin = require('../../configs/admin');
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+import { kill } from 'cross-port-killer';
+import $rest from '../../configs/rest.json';
+import $wot from '../../configs/wot.json';
+import $stream from '../../configs/stream.json';
 
+module.exports = $db => {
+  let serviceStatus;
+  const users = $db.users;
 
-module.exports = function ($db) {
-  var users = $db.users;
-
-  var startService = function(req, res, next) {
+  const startService = (req, res) => {
     global.startMCSLiteService();
-    this.serviceStatus = true;
-    return res.send(200, "success!");
+    serviceStatus = true;
+    return res.send(200, 'success!');
   };
 
-  var stopService = function(req, res, next) {
+  const stopService = (req, res) => {
     kill($rest.port);
     kill($wot.port);
     kill($stream.serverPort);
     kill($stream.rtmpServerPort);
-    this.serviceStatus = false;
-    return res.send(200, "success.");
+    serviceStatus = false;
+    return res.send(200, 'success.');
   };
 
-  var retrieveServiceSetting = function(req, res, next) {
+  const retrieveServiceSetting = (req, res) => {
     try {
-      var configFilePath = path.resolve(__dirname, '../../configs/' + req.params.settingId + '.json');
-      return fs.readFile(configFilePath, 'utf8', function(err, data) {
+      const configFilePath = path.resolve(
+        __dirname,
+        `../../configs/${req.params.settingId}.json`,
+      );
+      return fs.readFile(configFilePath, 'utf8', (err, data) => {
         if (err) throw err;
         return res.send(200, { data: JSON.parse(data) });
       });
     } catch (e) {
-      res.send(400, "Cannot find this file.");
+      return res.send(400, 'Cannot find this file.');
     }
   };
 
-  var editServiceSetting = function(req, res, next) {
-    var settingId = req.params.settingId;
-    return new Promise(function(resolve, reject) {
-      var content = req.body.content;
-      return fs.writeFile(path.resolve(__dirname, '../../configs/' + req.params.settingId + '.json'), JSON.stringify(content, null, 4), function(err) {
-        if (err) reject(err);
-        resolve();
-      });  
+  const editServiceSetting = (req, res) =>
+    new Promise((resolve, reject) => {
+      const content = req.body.content;
+      return fs.writeFile(
+        path.resolve(__dirname, `../../configs/${req.params.settingId}.json`),
+        JSON.stringify(content, null, 4),
+        err => {
+          if (err) reject(err);
+          resolve();
+        },
+      );
     })
-    .then(function() {
-      return res.send(200, "success.");
-    })
-    .catch(function(err) {
-      return res.send(400, err);
-    });
-  };
+      .then(() => res.send(200, 'success.'))
+      .catch(err => res.send(400, err));
 
-  var resetServiceSetting = function(req, res, next) {
-    return new Promise(function(resolve, reject) {
-      return exec('rm -rf ./configs/ && mkdir configs && cp -R ./defaultConfigs/* ./configs', { cwd: path.resolve(__dirname, '../../')}, function(error, stdout, stderr) {
-        if (error) reject(error);
-        resolve();
-      });
-    })
-    .then(function() {
-      return res.send(200, "success.");
-    })
-    .catch(function(err) {
-      return (400, err);
-    });
-  };
+  const resetServiceSetting = (req, res) =>
+    new Promise((resolve, reject) =>
+      exec(
+        'rm -rf ./configs/ && mkdir configs && cp -R ./defaultConfigs/* ./configs',
+        { cwd: path.resolve(__dirname, '../../') },
+        error => {
+          if (error) reject(error);
+          resolve();
+        },
+      ),
+    )
+      .then(() => res.send(200, 'success.'))
+      .catch(err => res.send(400, err));
 
-  var getServiceIp = function(req, res, next) {
-    var interfaces = os.networkInterfaces();
-    var addresses = [];
-    var restPath = path.resolve(__dirname, '../../configs/rest.json');
-    
-    return fs.readFile(restPath, 'utf8', function(err, data) {
-      var data = JSON.parse(data);
-      if (this.serviceStatus) {
-        for (var k in interfaces) {
-          for (var k2 in interfaces[k]) {
-            var address = interfaces[k][k2];
+  const getServiceIp = (req, res) => {
+    const interfaces = os.networkInterfaces();
+    const addresses = [];
+    const restPath = path.resolve(__dirname, '../../configs/rest.json');
+
+    return fs.readFile(restPath, 'utf8', (err, data) => {
+      const dataJSON = JSON.parse(data);
+      if (serviceStatus) {
+        Object.keys(interfaces).forEach(k => {
+          Object.keys(interfaces[k]).forEach(k2 => {
+            const address = interfaces[k][k2];
             if (address.family === 'IPv4' && !address.internal) {
-              addresses.push(address.address + ':' + data.port);
+              addresses.push(`${address.address}:${dataJSON.port}`);
             }
-          }
-        }
+          });
+        });
       }
       return res.send(200, { data: addresses });
     });
   };
 
-  var getServiceLog = function(req, res, next) {
-    return res.send(200, { data: JSON.stringify(global.logs) });
-  };
+  const getServiceLog = (req, res) =>
+    res.send(200, { data: JSON.stringify(global.logs) });
 
-  var resetData = function(req, res, next) {
-    users.retrieveAdminUsers()
-    .then(function(data) {
-      var adminUsersContent = '';
-      
-      data.forEach(function(k, v) {
-        adminUsersContent += k.toString() + '\n';
-      });
-      
-      fs.writeFileSync(path.resolve(__dirname, '../../db/users.json'), adminUsersContent, 'utf8');
-      fs.writeFileSync(path.resolve(__dirname, '../../db/datapoints.json'), '', 'utf8');
-      fs.writeFileSync(path.resolve(__dirname, '../../db/devices.json'), '', 'utf8');
-      fs.writeFileSync(path.resolve(__dirname, '../../db/prototypes.json'), '', 'utf8');
-      return res.send(200, "success.");
-    })
-    .catch(function(err) {
-      return (400, err);
-    });
+  const resetData = (req, res) => {
+    users
+      .retrieveAdminUsers()
+      .then(data => {
+        let adminUsersContent = '';
+
+        data.forEach(k => {
+          adminUsersContent += `${k.toString()}\n`;
+        });
+
+        fs.writeFileSync(
+          path.resolve(__dirname, '../../db/users.json'),
+          adminUsersContent,
+          'utf8',
+        );
+        fs.writeFileSync(
+          path.resolve(__dirname, '../../db/datapoints.json'),
+          '',
+          'utf8',
+        );
+        fs.writeFileSync(
+          path.resolve(__dirname, '../../db/devices.json'),
+          '',
+          'utf8',
+        );
+        fs.writeFileSync(
+          path.resolve(__dirname, '../../db/prototypes.json'),
+          '',
+          'utf8',
+        );
+        return res.send(200, 'success.');
+      })
+      .catch(err => res.send(400, err));
   };
 
   return {
     serviceStatus: false,
-    retrieveServiceSetting: retrieveServiceSetting,
-    editServiceSetting: editServiceSetting,
-    resetServiceSetting: resetServiceSetting,
-    getServiceIp: getServiceIp,
-    resetData: resetData,
-    getServiceLog: getServiceLog,
-    startService: startService,
-    stopService: stopService,
+    retrieveServiceSetting,
+    editServiceSetting,
+    resetServiceSetting,
+    getServiceIp,
+    resetData,
+    getServiceLog,
+    startService,
+    stopService,
   };
-
 };
