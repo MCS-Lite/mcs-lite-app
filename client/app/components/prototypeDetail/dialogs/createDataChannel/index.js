1f85e108743874fb77143bc59d7c2684e0d21375
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-10-27 11:20:18 +0800

    fix(Prototype): i18n for create dc list

--- client/app/components/prototypeDetail/dialogs/createDataChannel/index.js
+++ client/app/components/prototypeDetail/dialogs/createDataChannel/index.js
@@ -19,7 +19,7 @@ import withProps from 'recompose/withProps';
 import { withGetMessages } from 'react-intl-inject-hoc';
 import messages from '../../messages';
 import Dialog from '../../../common/dialog';
-import { data } from '../../../../utils/dataChannelTypes';
+import { getData } from '../../../../utils/dataChannelTypes';
 import Preview from '../../preview';
 
 import styles from './styles.css';
@@ -138,8 +138,8 @@ export default compose(
   withState('channelType', 'setChannelType', {}),
   withState('format', 'setFormat', {}),
   withState('error', 'setError', {}),
-  withProps(({ displayCardType }) => {
-    const treatedData = data.map((k) => {
+  withProps(({ displayCardType, getMessages: t }) => {
+    const treatedData = getData(t).map((k) => {
       const value = k.type === 1
         ? `${k.dataChannelTypeName}_Control`
         : `${k.dataChannelTypeName}_Display`;
